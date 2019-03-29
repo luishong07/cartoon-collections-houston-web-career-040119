21ef68984b7565311123c02d0670d8cfdf90ec6e
Luis Hong <luishong50@gmail.com>
2019-03-29 04:19:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,5 +29,5 @@ def find_the_cheese(contains_cheddar)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   contains_cheddar.find do |items|
-  cheese_types.include?()
+  cheese_types.include?(items)
 end
