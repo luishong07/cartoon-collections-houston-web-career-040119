d32b67b8cfcc91e2d6d113556401b175e19af9d8
Luis Hong <luishong50@gmail.com>
2019-03-29 03:33:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,31 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index {|d|n|}
-    #puts d
+  dwarves.each_with_index do |d,n| #d=dwarves , n=numbers
+  
+    puts " #{n+1}.#{d} "
   
   end
   
 end
 
-def summon_captain_planet# code an argument here
+planeteer= ["earth", "wind","fire", "water", "heart"] #veggies , fruits??? look at spec
+def summon_captain_planet(planeteer)# code an argument here
   # Your code here
+  planeteer.map {|plnt|  plnt.capitalize + '!'}
+  
 end
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
+  
+  #planeteer.any? {| plnt| plnt.length > 4 }
+  calls_long.any? {|w| w.length >4 }
+  
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
